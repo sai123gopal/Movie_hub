b70dcdb1df9062eca2ccc99867541bcfccac788b
Sai Gopal <45008991+sai123gopal@users.noreply.github.com>
2023-06-09 11:31:34 +0530

    Update Utils.java

--- app/src/main/java/com/saigopl/movie_hub/helpUtils/Utils.java
+++ app/src/main/java/com/saigopl/movie_hub/helpUtils/Utils.java
@@ -2,7 +2,7 @@ package com.saigopl.movie_hub.helpUtils;
 
 public class Utils {
 
-    public static final String apiKey = "aaa6d0289078c31a06c76d4c39d1c33b";
+    public static final String apiKey = "Null";
     public static final String language = "en-US";
 
 
